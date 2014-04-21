84a885b35aeb79a9ecd0afc6d1dd4eddaef81fba
Davey Shafik <me@daveyshafik.com>
2014-04-21 05:51:54 +0000

    Update layout/styles using bootstrap

--- public/index.php
+++ public/index.php
@@ -10,20 +10,50 @@ $topics = $data->getAllTopics();
 <html>
     <head>
         <title>Suggestotron</title>
+        <link rel="stylesheet" type="text/css" href="/css/bootstrap.min.css">
     </head>
     <body>
-        <?php
-        foreach ($topics as $topic) {
-            echo "<h3>" .$topic['title']. " (ID: " .$topic['id']. ")</h3>";
-            echo "<p>";
-            echo nl2br($topic['description']);
-            echo "</p>";
-            echo "<p>";
-            echo "<a href='/edit.php?id=" .$topic['id']. "'>Edit</a>";
-            echo " | ";
-            echo "<a href='/delete.php?id=" .$topic['id']. "'>Delete</a>";
-            echo "</p>";
-        }
-        ?>
+        <nav class="navbar navbar-default" role="navigation">
+            <div class="container-fluid">
+                <div class="navbar-header">
+                    <a class="navbar-brand" href="#">Suggestotron</a>
+                </div>
+                <form class="navbar-form navbar-right" role="search">
+                    <a href="/add.php" class="btn btn-default">
+                        <span class="glyphicon glyphicon-plus-sign"></span>
+                        Add Topic
+                    </a>
+                </form>
+            </div>
+        </nav>
+        <div class="container">
+            <?php
+            foreach ($topics as $topic) {
+                ?>
+                <section>
+                    <div class="row">
+                        <div class="col-xs-12">
+                            <h3><?=$topic['title'];?></h3>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-xs-8">
+                            <p class="text-muted">
+                                <?=nl2br($topic['description']);?>
+                            </p>
+                        </div>
+                        <div class="col-xs-4">
+                            <p class="pull-right">
+                                <a href="/edit.php?id=<?=$topic['id']; ?>" class="btn btn-primary">Edit</a>
+                                <a href="/delete.php?id=<?=$topic['id']; ?>" class="btn btn-danger">Delete</a>
+                            </p>
+                        </div>
+                    </div>
+                </section>
+                <hr>
+                <?php
+            }
+            ?>
+        </div>
     </body>
 </html>
