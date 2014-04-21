dceff2f4974764b77c5343450023ee2ca6d7ed4a
Davey Shafik <me@daveyshafik.com>
2014-04-21 19:42:02 +0000

    Remove uncessesary .php files

--- public/list.php
+++
@@ -1,8 +0,0 @@
-<?php
-$data = new \Suggestotron\TopicData();
-
-$topics = $data->getAllTopics();
-
-$template = new \Suggestotron\Template("../views/base.phtml");
-$template->render("../views/index/list.phtml", ['topics' => $topics]);
-?>
